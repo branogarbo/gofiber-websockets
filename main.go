1b14f6948e71c124df5370f1f7618cf25f102c3f
Brian Longmore <brianl.ext@gmail.com>
2021-01-29 14:30:27 -1000

    put WS code in util package, added middleware for fun

--- main.go
+++ main.go
@@ -1,91 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
+	"websockets/util"
 
-	"github.com/antoniodipinto/ikisocket"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/websocket/v2"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-type Message struct {
-	Data string `json:"data"`
-}
-
 func main() {
-	clients := make(map[string]string)
-
 	server := fiber.New()
+
+	server.Use(favicon.New())
+	server.Use(logger.New())
 
 	server.Static("/", "./public/index.html")
 
-	server.Use(func(c *fiber.Ctx) error {
-		if websocket.IsWebSocketUpgrade(c) {
-			c.Locals("allowed", true)
-			return c.Next()
-		}
+	util.SetupWS(server)
 
-		return fiber.ErrUpgradeRequired
+	server.Get("*", func(c *fiber.Ctx) error {
+		return c.Status(404).SendString("sorry bro, 404")
 	})
-
-	////////////////////////////////////
-
-	ikisocket.On(ikisocket.EventConnect, func(ep *ikisocket.EventPayload) {
-		fmt.Println(ep.Kws.GetAttribute("user_id"), "connected")
-	})
-
-	ikisocket.On(ikisocket.EventDisconnect, func(ep *ikisocket.EventPayload) {
-		delete(clients, ep.Kws.GetAttribute("user_id"))
-		fmt.Println(ep.SocketAttributes["user_id"], "disconnected")
-	})
-
-	ikisocket.On(ikisocket.EventClose, func(ep *ikisocket.EventPayload) {
-		delete(clients, ep.SocketAttributes["user_id"])
-		fmt.Println(ep.SocketAttributes["user_id"], "connection closed")
-	})
-
-	ikisocket.On(ikisocket.EventError, func(ep *ikisocket.EventPayload) {
-		fmt.Println("ikisocket error occurred")
-	})
-	ikisocket.On(ikisocket.EventMessage, func(ep *ikisocket.EventPayload) {
-		fmt.Println("recieved message from", ep.SocketAttributes["user_id"])
-
-		message := Message{}
-
-		err := json.Unmarshal(ep.Data, &message)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if message.Data == "ping" {
-			reply, err := json.Marshal(Message{
-				Data: "pong",
-			})
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			ep.Kws.Emit(reply)
-			fmt.Println("sent message to", ep.SocketAttributes["user_id"])
-		}
-
-	})
-
-	////////////////////////////////////
-
-	server.Get("/ws/:id", ikisocket.New(func(kws *ikisocket.Websocket) {
-		userId := kws.Params("id")
-
-		clients[userId] = kws.UUID
-
-		kws.SetAttribute("user_id", userId)
-
-		kws.Broadcast([]byte(fmt.Sprintf("%s connected", userId)), true)
-	}))
 
 	log.Fatal(server.Listen(":3004"))
 }
---
+++ util/websockets.go
@@ -0,0 +1,81 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/antoniodipinto/ikisocket"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+)
+
+func SetupWS(server *fiber.App) {
+	clients := make(map[string]string)
+
+	server.Use("/ws", func(c *fiber.Ctx) error {
+		fmt.Println("YAHHHHHHH")
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+
+		return fiber.ErrUpgradeRequired
+	})
+
+	////////////////////////////////////
+
+	ikisocket.On(ikisocket.EventConnect, func(ep *ikisocket.EventPayload) {
+		fmt.Println(ep.Kws.GetAttribute("user_id"), "connected")
+	})
+
+	ikisocket.On(ikisocket.EventDisconnect, func(ep *ikisocket.EventPayload) {
+		delete(clients, ep.Kws.GetAttribute("user_id"))
+		fmt.Println(ep.SocketAttributes["user_id"], "disconnected")
+	})
+
+	ikisocket.On(ikisocket.EventClose, func(ep *ikisocket.EventPayload) {
+		delete(clients, ep.SocketAttributes["user_id"])
+		fmt.Println(ep.SocketAttributes["user_id"], "connection closed")
+	})
+
+	ikisocket.On(ikisocket.EventError, func(ep *ikisocket.EventPayload) {
+		fmt.Println("ikisocket error occurred")
+	})
+	ikisocket.On(ikisocket.EventMessage, func(ep *ikisocket.EventPayload) {
+		fmt.Println("recieved message from", ep.SocketAttributes["user_id"])
+
+		message := Message{}
+
+		err := json.Unmarshal(ep.Data, &message)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if message.Data == "ping" {
+			reply, err := json.Marshal(Message{
+				Data: "pong",
+			})
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			ep.Kws.Emit(reply)
+			fmt.Println("sent message to", ep.SocketAttributes["user_id"])
+		}
+
+	})
+
+	////////////////////////////////////
+
+	server.Get("/ws/:id", ikisocket.New(func(kws *ikisocket.Websocket) {
+		userId := kws.Params("id")
+
+		clients[userId] = kws.UUID
+
+		kws.SetAttribute("user_id", userId)
+
+		kws.Broadcast([]byte(fmt.Sprintf("%s connected", userId)), true)
+	}))
+}
